2a57d8358ceb301f4620e9a10bad487a86736042
Harris Moustakas <harris.moustakas@ravenind.com>
2024-12-28 16:22:56 +0200

    Update Graphics utils on rendering models, updating and style refactor

--- include/GraphicsUtils.hpp
+++ include/GraphicsUtils.hpp
@@ -1,10 +1,11 @@
 #pragma once
-
 #include <GL/glew.h>
 
 #include <GL/gl.h>
+
 #include <string>
 
+#include <CameraHandler.hpp>
 #include <Mesh.hpp>
 
 namespace simulator {
@@ -17,7 +18,7 @@ enum GraphicsRes { FAIL = -1, SUCCESS = 1 };
  * @param texture_id
  * @return
  */
-GraphicsRes load_texture_from_image(std::string file_name, GLuint &texture_id);
+GraphicsRes loadTextureFromImage(std::string file_name, GLuint &texture_id);
 
 /**
  * @brief load_shaders
@@ -25,15 +26,34 @@ GraphicsRes load_texture_from_image(std::string file_name, GLuint &texture_id);
  * @param vertex_shader_path
  * @return
  */
-GraphicsRes load_shaders(const char *fragment_shader_path,
-                         const char *vertex_shader_path, GLuint &program_id);
+GraphicsRes loadShaders(const char *fragment_shader_path,
+                        const char *vertex_shader_path, GLuint &program_id);
 
 /**
  * @brief bind_to_GPU
  * @param model
  * @return
  */
-void bind_to_GPU(model::Model &model);
+void bindToGPU(model::Model &model);
 
+/**
+ * @brief render
+ * @param model
+ * @return
+ */
+GraphicsRes render(model::Model &model, GLuint view_id, glm::mat4 &view_matrix);
+
+/**
+ * @brief debugOpenGL
+ * @return
+ */
+void debugOpenGL();
+
+/**
+ * @brief updateOnEvents
+ * @param camera
+ * @param model_pos
+ */
+void updateOnEvents(CameraHandler &camera, glm::vec3 *model_pos = nullptr);
 } // namespace graphics_utils
 } // namespace simulator
